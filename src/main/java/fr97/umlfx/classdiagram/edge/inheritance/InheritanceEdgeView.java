eb4a09fc031054a91a1c9cbbfb236b1928f53147
FR97 <fristic97@gmail.com>
2023-09-06 22:20:41 +0200

    refactor: extract constant.

--- src/main/java/fr97/umlfx/classdiagram/edge/inheritance/InheritanceEdgeView.java
+++ src/main/java/fr97/umlfx/classdiagram/edge/inheritance/InheritanceEdgeView.java
@@ -10,8 +10,9 @@ import java.util.Arrays;
 
 public class InheritanceEdgeView extends AbstractEdgeView {
 
+    private static final double ARROW_ANGLE = Math.toRadians(25);
 
-    public InheritanceEdgeView(InheritanceEdge edge){
+    public InheritanceEdgeView(InheritanceEdge edge) {
         super(edge);
 
         calculatePositions(null, null, null);
@@ -26,14 +27,13 @@ public class InheritanceEdgeView extends AbstractEdgeView {
     protected Group createShape(double startX, double startY, double endX, double endY) {
         Group group = new Group();
 
-        double phi = Math.toRadians(25);
         int barb = 20;
         double dy = startY - endY;
         double dx = startX - endX;
         double theta = Math.atan2(dy, dx);
         double x = 0;
         double y = 0;
-        double rho = theta + phi;
+        double rho = theta + ARROW_ANGLE;
 
         double[] xs = new double[2];
         double[] ys = new double[2];
@@ -43,7 +43,7 @@ public class InheritanceEdgeView extends AbstractEdgeView {
             y = startY - barb * Math.sin(rho);
             xs[j] = x;
             ys[j] = y;
-            rho = theta - phi;
+            rho = theta - ARROW_ANGLE;
         }
 
         Polygon background = new Polygon();
@@ -56,7 +56,7 @@ public class InheritanceEdgeView extends AbstractEdgeView {
         Line line2 = new Line(startX, startY, xs[1], ys[1]);
         Line line3 = new Line(xs[0], ys[0], xs[1], ys[1]);
         shapeLines.addAll(Arrays.asList(line1, line2, line3));
-        shapeLines.forEach(l ->  l.setStrokeWidth(1));
+        shapeLines.forEach(l -> l.setStrokeWidth(1));
         group.getChildren().addAll(background, line1, line2, line3);
 
         group.setUserData("shape");
