4a1bbf49dde7e5fba00daf1431443c79f3e784f9
FR97 <fristic97@gmail.com>
2023-09-08 23:55:15 +0200

    chore: import clean up.

--- src/main/java/fr97/umlfx/classdiagram/ClassDiagramView.java
+++ src/main/java/fr97/umlfx/classdiagram/ClassDiagramView.java
@@ -1,10 +1,6 @@
 package fr97.umlfx.classdiagram;
 
-import fr97.umlfx.api.UmlDiagram;
 import fr97.umlfx.common.AbstractDiagramView;
-import fr97.umlfx.views.FXMLView;
-import fr97.umlfx.views.View;
-import javafx.scene.layout.Pane;
 
 public class ClassDiagramView extends AbstractDiagramView {
 
