e6579a915372b330f734b1e1fdde134906d13e4b
FR97 <fristic97@gmail.com>
2023-09-16 22:08:24 +0200

    chore: formatting.

--- src/main/java/fr97/umlfx/workspace/toolbar/ToolbarController.java
+++ src/main/java/fr97/umlfx/workspace/toolbar/ToolbarController.java
@@ -35,7 +35,6 @@ public class ToolbarController implements FXMLController<Toolbar> {
         onModelSet();
     }
 
-
     private void onModelSet() {
         toolBar.getItems().addAll(toolbarModel.getTools().stream()
                 .sequential()
